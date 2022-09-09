a405867507fc83f48eb26eb0ab8e13d99bc16f14
shiloh-theo <shilohtheophilus45@gmail.com>
2022-09-08 21:43:37 -0700

    4d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - print the string in the put function
  * Return: 0
  */
-
 int main(void)
-
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
