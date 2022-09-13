3e741ba652ee8d978cbaa2b9e1273f4101b8a46d
shiloh-theo <shilohtheophilus45@gmail.com>
2022-09-12 18:16:43 -0700

    100

---
+++ 0x00-hello_world/sample.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-main block, this function should genarate assembly code
+ * return 0
+ */
+
+int main(){
+	printf("this is a sample code for the asembly code task");
+
+}
