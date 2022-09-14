4a138bc814371461ec27131d3a6e8c8dccfbe51e
shiloh-theo <shilohtheophilus45@gmail.com>
2022-09-14 02:18:36 -0700

    no2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,18 @@
 #include"main.h"
 
 /**
- *  * print_alphabet_x10 - function that will print the alphabet
- *   *
- *   */
+ * print_alphabet_x10 - function that will print the alphabet
+ *
+*/
 
 void print_alphabet_x10(void)
 {
-		int line, ch;
+	int line, ch;
 
-			for (line = 0; line <= 9; ++line)
-					{
-								for (ch = 'a'; ch <= 'z'; ++ch)
-												putchar(ch);
-										putchar('\n');
-											}
+	for (line = 0; line <= 9; ++line)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
+			_putchar(ch);
+		_putchar('\n');
+	}
 }
