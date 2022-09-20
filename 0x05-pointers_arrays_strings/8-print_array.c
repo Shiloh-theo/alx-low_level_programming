dc2fa7ff41b02dbb12d3760e7dacfb7fd0a8ded5
shiloh-theo <shilohtheophilus45@gmail.com>
2022-09-19 20:46:08 -0700

    8.

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_array - function that prints the elements of an array
+ *
+ * @a: pointer variable to the array
+ *
+ * @n: number of elements to be printed from array
+ *
+ * Return: Return type is void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
